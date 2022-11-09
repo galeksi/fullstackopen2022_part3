50636a408df0838ebdf9a87469627d2f5113e436
Aleksi Rendel <aleksi.rendel@gmail.com>
2022-11-09 11:55:44 +0200

    Finished exercises 3.3-3.8

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
+
+morgan.token('body', (req) => {
+    if (req.method === 'POST') return JSON.stringify(req.body)
+})
+
+app.use(express.json())
+app.use(morgan((tokens, req, res) => {
+    return [
+        tokens.method(req, res),
+        tokens.url(req, res),
+        tokens.status(req, res),
+        tokens.res(req, res, 'content-length'), '-',
+        tokens['response-time'](req, res), 'ms',
+        tokens.body(req)
+    ].join(' ')
+}))
 
 let persons = [
     {
@@ -34,6 +51,44 @@ app.get('/info', (request, response) => {
 
 app.get('/api/persons', (request, response) => {
     response.json(persons)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(pers => pers.id === id)
+
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
+})
+
+app.post('/api/persons', (request, response) => {
+    const newPerson = request.body
+    console.log(newPerson)
+
+    if (!newPerson.name || !newPerson.number) {
+        return response.status(400).json({
+            error: 'name or number missing'
+        })
+    } else if (persons.find(pers => pers.name === newPerson.name)) {
+        return response.status(400).json({
+            error: `${newPerson.name} already in the phonebook`
+        })
+    }
+    newPerson.id = Math.floor(Math.random() * 100)
+
+    persons = persons.concat(newPerson)
+
+    response.json(newPerson)
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    persons = persons.filter(person => person.id !== id)
+
+    response.status(204).end()
 })
 
 const PORT = 3001
