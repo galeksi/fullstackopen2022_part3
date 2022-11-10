8fa895f586efd8b19c307ade59fc40c3d54704a0
Aleksi Rendel <aleksi.rendel@gmail.com>
2022-11-10 16:08:40 +0200

    Finished exercises 3.12-3.14

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const app = express()
+const Person = require('./models/person')
 
 morgan.token('body', (req) => {
     if (req.method === 'POST') return JSON.stringify(req.body)
@@ -21,50 +23,55 @@ app.use(morgan((tokens, req, res) => {
     ].join(' ')
 }))
 
-let persons = [
-    {
-        "id": 1,
-        "name": "Arto Hellas",
-        "number": "040-123456"
-    },
-    {
-        "id": 2,
-        "name": "Ada Lovelace",
-        "number": "39-44-5323523"
-    },
-    {
-        "id": 3,
-        "name": "Dan Abramov",
-        "number": "12-43-234345"
-    },
-    {
-        "id": 4,
-        "name": "Mary Poppendieck",
-        "number": "39-23-6423122"
-    }
-]
+// let persons = [
+//     {
+//         "id": 1,
+//         "name": "Arto Hellas",
+//         "number": "040-123456"
+//     },
+//     {
+//         "id": 2,
+//         "name": "Ada Lovelace",
+//         "number": "39-44-5323523"
+//     },
+//     {
+//         "id": 3,
+//         "name": "Dan Abramov",
+//         "number": "12-43-234345"
+//     },
+//     {
+//         "id": 4,
+//         "name": "Mary Poppendieck",
+//         "number": "39-23-6423122"
+//     }
+// ]
 
 app.get('/', (request, response) => {
     response.send('<h1>Phonebook backend application</h1>')
 })
 
 app.get('/info', (request, response) => {
-    response.send(`<h1>Phonebook has info for ${persons.length} people</h1><br /><h3>${new Date()}</h3>`)
+    Person.countDocuments({}, (err, personCount) => {
+        if (err) { return handleError(err) }
+        response.send(`<h1>Phonebook has info for ${personCount} people</h1><br /><h3>${new Date()}</h3>`)
+    })
 })
 
 app.get('/api/persons', (request, response) => {
-    response.json(persons)
+    Person.find({}).then(persons =>
+        response.json(persons)
+    )
 })
 
 app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const person = persons.find(pers => pers.id === id)
-
-    if (person) {
-        response.json(person)
-    } else {
-        response.status(404).end()
-    }
+    const id = request.params.id
+    Person.findById(id).then( person => {
+        if (person) {
+            response.json(person)
+        } else {
+            response.status(404).end()
+        }
+    })
 })
 
 app.post('/api/persons', (request, response) => {
@@ -80,21 +87,25 @@ app.post('/api/persons', (request, response) => {
             error: `${newPerson.name} already in the phonebook`
         })
     }
-    newPerson.id = Math.floor(Math.random() * 100)
-
-    persons = persons.concat(newPerson)
-
-    response.json(newPerson)
+    const person = new Person({
+        name: newPerson.name,
+        number: newPerson.number
+    })
+    person.save().then(savedPerson => {
+        response.json(savedPerson)
+    })
 })
 
 app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(person => person.id !== id)
-
-    response.status(204).end()
+    const id = request.params.id
+    Person.findById(id).then(person => {
+        console.log(person)
+        person.remove()
+        response.status(204).end()
+    })
 })
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
